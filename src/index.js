f5b38d5901ea3821b4116f5d73de2b7142568ece
stryakr <rnsteel1@asu.edu>
2017-03-11 14:40:59 -0800

    Reorg, modifications #PostHandler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 var Alexa = require('alexa-sdk');
 
 var states = {
-    QUERY: '_QUERYMODE',    // Ask the user what they looking for. TODO: Saae the attributes to DynamoDB
-    SEARCH: '_SEARCHMODE',  // Look for results in JSON
-    RESULT: '_RESULTMODE'   // Build the results for the user and reply TODO: Find a heuristics to filter data.
-};
+    QUERYSTATE:  '_STATEMODE',
+    QUERYCITY:   '_CITYMODE',
+    QUERYCO:     '_COMPMODE',
+    QUERYSKILL:  '_SKILLMODE',
+    SEARCH:      '_SEARCHMODE',  // Look for results in JSON
+    RESULT:      '_RESULTMODE'   // Build the results for the user and reply      TODO: Find a heuristics to filter data.
+
+    };
 var handlers = {
 
     'NewSessionIntent': function () {
-        this.emit(':ask', 'What state would you like to look for a job? ');
+        this.emit(':ask', 'Hi, I\'m Job Patrol; I will help you find a job wherever you want. \n' +
+            'What state do you want to look for a job?');
     },
 
     'WithStateIntent': function () {
@@ -30,8 +35,42 @@ var handlers = {
     'Unhandled' : function () {
         this.emit(':tell', 'Please try another command');
     }
-    };
+};
 
+var newSessionHandlers = {
+    // This will stop any other requests and blank out the requests
+
+    'NewSession': function() {
+        if(Object.keys(this.attributes).length === 0) { // Check if it's the first time the skill has been invoked
+            this.attributes['state']     = undefined;
+            this.attributes['city']      = undefined;
+            this.attributes['company']   = undefined;
+            this.attributes['skill']     = undefined;
+        }
+        this.handler.state = states._STATEMODE;
+        this.emit('NewSessionIntent');
+    }
+
+};
+
+var sessionHandlers = {
+    // This set of handlers will deal with incorporating new data and existing data.
+    'GetStateIntent': function() {
+        if (this.handler.state === states._STATEMODE) {
+        var inputState = parseString(this.event.request.intent.slots.state.value);
+        this.attributes['state'] = inputState;
+        console.log(inputState + " - inputState");
+        this.emit(':tell','Alright we will use ' + inputState);)
+        }
+    this.handler.state = states._CITYMODE;
+    }
+  },
+  'GetCityIntent' : function() {
+    if (this.handler.state === states._CITYMODE) {
+      var inputState
+    }
+  }
+}
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
