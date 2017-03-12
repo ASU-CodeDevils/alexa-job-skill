e7cacc19f6a664f991c6daa34a8400f0b25204ab
stryakr <rnsteel1@asu.edu>
2017-03-11 20:56:00 -0800

    Working request input #Part1

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ var states = {
     SEARCH: '_SEARCHMODE',  // Look for results in JSON
     RESULT: '_RESULTMODE'   // Build the results for the user and reply TODO: Find a heuristics to filter data.
 };
+
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+
 var handlers = {
 
     'NewSessionIntent': function () {
-        this.emit(':ask', 'What state would you like to look for a job? ');
+        this.emit(':ask', 'Welcome to Job Dog, What state would you like to look for a job?');
     },
 
     'WithStateIntent': function () {
@@ -27,18 +36,14 @@ var handlers = {
     'SessionEndRequest' : function () {
         this.emit(':tell', 'Thanks for using Job Finder');
     },
-
     'Unhandled' : function () {
-        this.emit(':tell', 'Please try another command');
-    ,
+        this.emit(':ask ', 'Please try another command');
+    },
     'PizzaBox' : function () {
-        this.emit(':tell', 'Ren is a bully')
-    }}
-
+        this.emit(':tell', 'Ren is a bully');
+    },
+    'GetStateIntent' : function() {
+          var slotValue = this.event.request.intent.slots.state.value;
+          this.emit(':ask', 'so you want to work in '+ slotValue);
+    }
     };
-
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
