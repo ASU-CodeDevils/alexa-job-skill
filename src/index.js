64652f1cc4293f0478135da5286603493eed5fc7
stryakr <rnsteel1@asu.edu>
2017-03-12 09:28:23 -0700

    API working #PART3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var Alexa = require('alexa-sdk');
 var http = require('http');
-var AppID = "amzn1.ask.skill.fd6e06df-7335-4be7-84a7-6292055fee0b";
+var App_ID = "amzn1.ask.skill.fd6e06df-7335-4be7-84a7-6292055fee0b";
 var baseUrl = 'http://service.dice.com/api/rest/jobsearch/v1/simple.json?';
 var states = {
 
@@ -10,15 +10,41 @@ var states = {
     RESULT:  'RESULT MODE'    // Build the results for the user and reply TODO: Find a heuristics to filter data.
 
 };
+var getAPI = function(callback) {
+    console.log("IN HARDCODE");
+    http.get(baseUrl + "state=Alaska", function(res){
+      console.log("IN http.get");
+          var body = '';
+          res.on('data', function(data){
+            console.log("IN res.data");
+              body += data;
+          });
+          res.on('end', function(){
+            console.log("IN res.end");
+            var result = JSON.parse(body);
+            console.log(result.count);
 
+            callback(result);
+          });
+        }).on('error', function(e){
+          console.log('Error: ' + e);
+        });
+}
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = AppID;
+    alexa.APP_ID = App_ID;
     alexa.registerHandlers(newSessionHandlers, defaultHandlers, queryHandlers);
     alexa.execute();
 };
 
 var newSessionHandlers = {
+    'HardCode' : function() {
+        var that = this;
+        getAPI(function(result) {
+          var count = result.count;
+          that.emit(':tell', "the number of responses was " + count);
+        })
+    },
     'NewSessionIntent': function () {
         this.handler.state = states.DEFAULT;
         this.emit(':ask', 'Welcome to Job Dog, What state would you like to look for a job?')
@@ -46,9 +72,6 @@ var newSessionHandlers = {
   };
 
 var defaultHandlers = Alexa.CreateStateHandler(states.DEFAULT, {
-      'YesIntent' : function() {
-        this.emit(':ask', 'was there a zip code that you wanted to work with?');
-      },
       'NoIntent' : function() {
         this.emit(':ask', 'Would you like me to perform the search now?' );
         this.handler.state = states.QUERY;
@@ -62,16 +85,16 @@ var defaultHandlers = Alexa.CreateStateHandler(states.DEFAULT, {
       'SessionEndRequest' : function () {
           this.emit(':tell', 'Thanks for using Job Finder');
       },
-        'YesIntent' : function() {
-          this.handler.state = states.QUERY;
-          this.emit(':ask', 'Awesome, let me run that search for you');
+      'YesIntent' : function() {
+          this.emit(':ask', 'Awesome, let me run that search for you. We are leaving Default State');
       },
-        'Unhandled' : function () {
+      'Unhandled' : function () {
           this.emit(':ask ', 'Please try another command');
       },
       'GetStateIntent' : function() {
             var slotValue = this.event.request.intent.slots.state.value;
             this.handler.state = states.QUERY;
+            this.attributes["state"] = slotValue;
             this.emit(':ask', 'so you want to work in Nantuket');
       }
   });
@@ -81,21 +104,12 @@ var defaultHandlers = Alexa.CreateStateHandler(states.DEFAULT, {
         this.emit(':ask', 'Was there something else you wanted to add to the search parameter?');
       },
       'YesIntent' : function() {
-          http.get(baseUrl, function(res){
-            var body = '';
-
-            res.on('data', function(data){
-              body += data;
-            });
-            res.on('end', function(){
-              var result = JSON.parse(body);
-              this.emit(':tell', 'there are ' + result.count + ' number of results');
-            });
-          }).on('error', function(e){
-            console.log('Error: ' + e);
-          });
-        },
+        request(baseUrl + "state=" + this.attributes["state"], function(body) {
+          var json = JSON.parse(body);
+          this.emit(':tell','The number of results are ' + json.count );
+        })
+      },
       'DefaultIntent' : function() {
         this.emit(':tell', 'Made it to Query State');
       }
-});
+  });
